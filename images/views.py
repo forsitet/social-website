975e8dcc48e311a3a7678671a2a75e63e5db638b
Denis Budin <D20041152@yandex.ru>
2024-04-23 04:00:57 +0300

    added likes

--- images/views.py
+++ images/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from django.shortcuts import get_object_or_404
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @login_required
 def image_create(request):
@@ -35,3 +37,22 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, "images/image/detail.html", {"section": "images",
                                                         "image": image})
+
+
+@login_required
+@require_POST
+def image_like(requset):
+    image_id = requset.POST.get("id")
+    action = requset.POST.get("action")
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                image.users_like.add(requset.user)
+            else:
+                image.users_like.remove(requset.user)
+            return JsonResponse({"status": "ok"})
+        
+        except Image.DoesNotExist:
+            pass
+        return JsonResponse({"status": "error"})
