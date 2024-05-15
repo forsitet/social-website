8e547b960ebcfce1f9d2dc0e5815512d1b3b502b
Denis Budin <D20041152@yandex.ru>
2024-05-16 01:29:12 +0300

    added images ranking

--- images/views.py
+++ images/views.py
@@ -50,6 +50,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # Увеличить общее число просмторов на 1
     total_views = r.incr(f"image:{image.id}:views")
+    # увеличить рейтинг на 1
+    r.zincrby("image_ranking", 1, image.id)
     return render(request, "images/image/detail.html", {"section": "images",
                                                         "image": image,
                                                         "total_views": total_views})
@@ -105,3 +107,16 @@ def image_list(request):
                   "images/image/list.html",
                   {"section": "images",
                    "images": images})
+
+
+@login_required
+def image_ranking(request):
+    # получить словарь рейтинга изображений
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # получить наиболее просматриваемые изображения
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, "images/image/ranking.html",
+                  {"section": "images",
+                   "most_viewed": most_viewed})
